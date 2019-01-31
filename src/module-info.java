42a6591acae42eff2c15440bacbbcbf82fe38d8c
bastiseen <bastiseen@bastiseen-SVF1421E2EW>
2019-01-31 22:17:32 +0000

    Set up

---
+++ src/module-info.java
@@ -0,0 +1,13 @@
+/**
+ * @author bastiseen
+ *
+ */
+module HeadFirstJava {
+	exports bastiseen.training;
+	exports bastiseen.headfirstjava;
+
+	requires java.desktop;
+	requires javafx.base;
+	requires javafx.controls;
+	requires javafx.graphics;
+}
